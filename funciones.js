23ea72bc38f51ef8393cd52bf2a75f7bda970a6b
william <williamva111000@gmail.com>
2023-05-08 19:12:30 -0500

    cph1

---
+++ funciones.js
@@ -0,0 +1,10 @@
+//Función: sumaTres
+var sumaTres = (x) => {
+    return x + 3;
+  }
+
+  //Función: cuidadoConElConsoleLog
+function cuidadoConElConsoleLog(nombre) {
+    console.log(nombre);
+    return nombre;
+  }
